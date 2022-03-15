4eee771f169b1c0dbdb90044397b40b3ab4f5d82
hammadnawaz35@gmail.com <hammad12345@>
2022-03-15 19:47:39 +0500

    Second commit

--- app/src/main/java/com/ui/signinfirebase/MainActivity2.java
+++ app/src/main/java/com/ui/signinfirebase/MainActivity2.java
@@ -1,18 +1,47 @@
 package com.ui.signinfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity2 extends AppCompatActivity {
 Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+//                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            Toast.makeText(MainActivity2.this, "Fetching FCM registration token failed" ,Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+//                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity2.this, "your token is "+token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
         button=findViewById(R.id.btnlsignIn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
